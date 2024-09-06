b24659efa287036ef5274344499c9248f9c06ef8
Christian Thorby <christianthorby@Christians-MacBook-Air.local>
2024-09-06 19:36:13 +0200

    tried removing warning hints, didnt work

--- init.lua
+++ init.lua
@@ -610,7 +610,14 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
-        rust_analyzer = {},
+        rust_analyzer = {
+          ['rust-analyzer'] = {
+            check = {
+              ignore = {
+                'dead_code',
+              },
+            },
+          },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
